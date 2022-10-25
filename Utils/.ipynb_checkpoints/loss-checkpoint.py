79a2a27de0e24979ee9e3af6d00f80cc0925d699
LuisWohlers <luis-wohlers@gmx.net>
2022-10-25 21:12:50 +0200

    25102022_1

--- Utils/.ipynb_checkpoints/loss-checkpoint.py
+++ Utils/.ipynb_checkpoints/loss-checkpoint.py
@@ -2,18 +2,40 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+def dice_score(inputs,targets,activation,smooth=1):
+    if activation is not None:
+        inputs = activation(inputs)
+    
+    #inputs = inputs.view(-1)
+    #targets = targets.view(-1)
+        
+    intersection = (inputs * targets).sum()                            
+    return (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)
+    
+    
+
 class DiceLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(DiceLoss, self).__init__()
  
     def forward(self, inputs, targets, smooth=1):        
         
-        inputs = F.sigmoid(inputs)       
+        activation = torch.sigmoid       
         
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
-        
-        intersection = (inputs * targets).sum()                            
-        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        dice = dice_score(inputs,targets,activation)
         
         return 1 - dice
+    
+class BCEDiceLoss(DiceLoss):
+    __name__ = 'bce_dice_loss'
+
+    def __init__(self, lambda_dice=1.0, lambda_bce=1.0):
+        super().__init__()
+        self.bce = nn.BCEWithLogitsLoss(reduction='mean')
+        self.lambda_dice=lambda_dice
+        self.lambda_bce=lambda_bce
+
+    def forward(self, y_pr, y_gt):
+        dice = super().forward(y_pr, y_gt)
+        bce = self.bce(y_pr, y_gt)
+        return (self.lambda_dice*dice) + (self.lambda_bce* bce)
